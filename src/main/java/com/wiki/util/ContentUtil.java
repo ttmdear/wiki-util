0c2fb2bf49ecd03058086d92f2e41b13d8fe0602
Paweł Bobryk <bobryk.pawel@gmail.com>
2021-10-22 15:03:26 +0200

    Update.

--- src/main/java/com/wiki/util/ContentUtil.java
+++ src/main/java/com/wiki/util/ContentUtil.java
@@ -2,10 +2,10 @@ package com.wiki.util;
 
 public class ContentUtil {
     public static void appendLine(StringBuilder stringBuilder, String... content) {
-        stringBuilder.append("\n");
-
         for (String s : content) {
             stringBuilder.append(s);
         }
+
+        stringBuilder.append("\n");
     }
 }
